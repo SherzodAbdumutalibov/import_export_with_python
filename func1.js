55051a2e3d94d0e7eb349b8dfcedf517da4720e1
Шерзоджон Абдумуталибов <sherzodjon.abdumutalibov@fido.uz>
2024-03-29 17:59:59 +0500

    first commit

---
+++ func1.js
@@ -0,0 +1,12 @@
+function sayHi() {
+    alert('Работает import и export');
+}
+
+export { sayHi };
+
+
+export const sto = 100500;
+export const slovo = 'йцукен';
+
+
+
---
+++ main.js
@@ -0,0 +1,17 @@
+import { sayHi } from "./func1.js";
+
+sayHi();
+
+
+import { sto } from "./func1.js";
+
+console.log(sto);
+
+
+import { slovo } from "./func1.js";
+
+console.log(slovo);
+
+let b = sto;
+console.log(b)
+
